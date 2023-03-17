6059aa01f653470291919bc379ccf971d07fabaf
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-17 01:36:49 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let multiply;
+const num1 = 2;
+const num2 = 31;
+multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()) + 1;
+
+let mod;
+const num3 = 30;
+const num4 = 13;
+mod = num3 % num4;
+
+let max = Math.max(1, 15, 13, 9, 19, 20, 17, 3);
+
